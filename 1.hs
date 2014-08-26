edebb28252036f693fd26f35b5d6e5f97a6f45d8
Duncan Brown <duncanphilipbrown@hotmail.com>
2014-08-26 21:57:28 +0100

    fix1

--- 1.hs
+++ 1.hs
@@ -1,3 +1,3 @@
 answer = sum $ filter multipleOf3Or5 [1..999]
           where multipleOf3Or5 n = (n`multipleOf`3) || (n`multipleOf`5)
-                multipleOf = (==0) . mod
+                x `multipleOf` y = x `mod` y == 0
