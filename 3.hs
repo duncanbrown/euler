5a4199f70c9ce02d615eb1722c1a1c919f712b9e
Duncan Brown <duncanphilipbrown@hotmail.com>
2014-08-28 22:08:05 +0100

    31

--- 3.hs
+++ 3.hs
@@ -1,7 +1,11 @@
-factors :: Integer -> [Integer]
-factors n = [x | x <- [1..n], n `mod` x == 0]
-
 isPrime :: Integer -> Bool
-isPrime n = length (factors n) == 2
+isPrime n = let sqrtN = floor $ sqrt $ fromIntegral n
+                testFactors = 2 : [3,5..sqrtN]
+            in null [x | x <- testFactors, x `isFactorOf` n]
 
-answer = maximum $ filter isPrime $ factors 600851475143
+isFactorOf :: Integer -> Integer -> Bool
+x `isFactorOf` y = y `mod` x == 0
+
+answer = let n = 600851475143
+             start = floor (fromIntegral n/3)
+         in head [x | x <- [start,start-2..1], x `isFactorOf` n && isPrime x]
